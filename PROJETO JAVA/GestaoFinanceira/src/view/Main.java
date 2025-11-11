2fcbb332845987041052d2278b2e5fa6f2a31841
sofl-aline <soaline.di@gmail.com>
2025-11-11 08:18:01 -0300

    Delete PROJETO JAVA directory

--- PROJETO JAVA/GestaoFinanceira/src/view/Main.java
+++
@@ -1,9 +0,0 @@
-package view;//Sistema Financeiro Pessoal
-//Grupo: Aline Silva, Camilly Beatriz, Amanda Diniz e Maria Isabelly
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-}
